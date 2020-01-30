a214b95220096d2171b523bdb9037c4e96864763
Your Name <you@example.com>
2020-01-29 23:55:30 -0500

    Updated program with iteration logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//
+// Author: Ettienne Voges
+// Program Week 3 Iteration program
+//
+//
+using System;
 
 namespace Deliverable2Iterative
 {
@@ -6,7 +11,33 @@ namespace Deliverable2Iterative
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Ask the user to enter an integer value between 1 and 100.
+            Console.WriteLine("Enter an integer value between 1 and 100:");
+
+            // Read the grade from standard input
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            // Exit with 1 failure exit code if the user ignore the bracket of accepted integers requested
+            if (count < 1 || count > 100)
+            {
+                Console.WriteLine("Require an integer value between 1 and 100.. Exiting.");
+                Environment.Exit(1);
+            }
+            
+            // Using a for loop, display a  message showing the number of times the user has entered 
+            // for the integer value: 
+            // "You have entered #. This is the current integer value in the loop: #
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine("You have entered {0}. This is the current integer value in the loop: {1}", count , i);
+            }
+
+            // Read bogus input to give the user a change to see the output 
+            // Console.ReadLine();
+
+            // Exit with 0 success exit code
+            Environment.Exit(0);
         }
     }
 }
